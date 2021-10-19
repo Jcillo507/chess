824ff6240b45ad014e33e858c5c7a83e39abdbcf
Joe Cillo <JCillo507@yahoo.com>
2021-10-19 14:54:09 -0400

    pieces showing

--- src/components/Piece.jsx
+++ src/components/Piece.jsx
@@ -1,15 +1,14 @@
-import React from 'react'
+import React from "react";
 import piece from "./assets/r_w.png";
 
-const Piece = ({piece:{type, color}}) => {
-  console.log(type,color)
-  console.log(`./assets/${type}_${color}.png`);
- const pieceImg = require(`./assets/${type}_${color}.png`);
+const Piece = ({ piece: { type, color } }) => {
+  const pieceImg = require(`./assets/${type}_${color}.png`).default;
+
   return (
     <div>
       <img src={pieceImg} alt="piece" />
     </div>
   );
-}
+};
 
-export default Piece
+export default Piece;
