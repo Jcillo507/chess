cc6b60ff8e937072bf57bc7cb30abb888c3a414f
Joe Cillo <JCillo507@yahoo.com>
2021-10-19 20:55:55 -0400

    can promote pawns

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from 'react'
 
 import './App.css';
-import { gameSubject } from './components/Game'
+import { gameSubject, initGame } from './components/Game'
 import Board from './components/Board'
 
 const App = () => {
   const [board, setBoard] = useState([])
   useEffect(() => {
+    initGame()
     const subscribe = gameSubject.subscribe((game) => setBoard(game.board))
     return () => subscribe.unsubscribe()
   }, [])
--- src/components/Game.js
+++ src/components/Game.js
@@ -1,17 +1,53 @@
 import * as Chess from 'chess.js'
 import { BehaviorSubject } from 'rxjs'
 
-const chess = new Chess()
 
-export const gameSubject = new BehaviorSubject({
-  board: chess.board()
-})
+let promotion = 'rnb2bnr/pppPkppp/8/4p3/7q/8/PPPP1PPP/RNBQKBNR w KQ - 1 5'
 
-export const move = (from, to) => {
-  const legalMove = chess.move({ from, to })
+const chess = new Chess(promotion)
+
+// const chess = new Chess()
+
+export const gameSubject = new BehaviorSubject()
+
+
+export const move = (from, to, promotion) => {
+  let tempMove = { from, to }
+  if (promotion) {
+    tempMove.promotion = promotion
+  }
+
+  const legalMove = chess.move(tempMove)
   if (legalMove) {
     gameSubject.next({
       board: chess.board()
     })
   }
+}
+
+export const handleMove = (from, to) => {
+  const promotions = chess.moves({ verbose: true }).filter(m => m.promotion)
+  console.table(promotions)
+  if (promotions.some(p => `${p.from}:${p.to}` === `${from}:${to}`)) {
+    const pendingPromotion = { from, to, color: promotions[0].color }
+    updateGame(pendingPromotion)
+  }
+  const { pendingPromotion } = gameSubject.getValue()
+  if (!pendingPromotion) {
+    move(from, to)
+  }
+
+  move(from, to)
+}
+
+const updateGame = (pendingPromotion) => {
+  const newGame = {
+    board: chess.board(),
+    pendingPromotion
+  }
+  gameSubject.next(newGame)
+}
+
+export const initGame = () => {
+  updateGame()
 }
