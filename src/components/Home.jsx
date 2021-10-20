c56ff37b8b1186570ca131edf40bb4c928294dd4
Joe Cillo <JCillo507@yahoo.com>
2021-10-20 16:05:11 -0400

    firebase set up for online play

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,74 @@
-import React from 'react'
+import React, { useState } from "react";
+import {useHistory} from 'react-router-dom'
+import { auth, db } from "../firebase";
 
 const Home = () => {
-  return (
-    <div>
-      home
-    </div>
-  )
-}
+  const { currentUser } = auth;
+  const [showModal, setShowModal] = useState(false);
+  const history = useHistory()
 
-export default Home
+  const newGameOptions = [
+    { label: "Black Pieces", value: "b" },
+    { label: "White Pieces", value: "w" },
+    { label: "Random", value: "r" },
+  ];
+  const handlePlayOnline = () => {
+    setShowModal(true);
+  };
+
+  const startOnlineGame = async (startingPiece) => {
+    const member = {
+      uid: currentUser.uid,
+      piece:
+        startingPiece === "Random"
+          ? ["b", "w"][Math.round(Math.random())]
+          : startingPiece,
+      name: localStorage.getItem("userName"),
+      creator: true,
+    };
+    const game ={
+      status: 'waiting',
+      members: [member],
+      gameId: `${Math.random().toString(36).substr(2, 9)}_${Date.now()}`
+    }
+     await db.collection('games').doc(game.gameId).set(game)
+     history.push(`/game/${game.gameId}`)
+
+
+  };
+  return (
+    <>
+      <div className="columns home">
+        <div className="column has-background-primary home-col">
+          <button className="button is-link">Play Locally</button>
+        </div>
+        <div className="column has-background-link home-col">
+          <button className="button is-primary" onClick={handlePlayOnline}>
+            Play Online
+          </button>
+        </div>
+        <div className={`modal ${showModal ? "is-active" : ""}`}>
+          <div className="modal-background"></div>
+          <div className="modal-content">
+            <div className="card">
+              <div className="card-content">Select your color</div>
+              <footer className="card-footer">
+                {newGameOptions.map(({ label, value }) => (
+                  <span className="card-footer-item pointer" key={value} onClick={()=>startOnlineGame(value)}>
+                    {label}
+                  </span>
+                ))}
+              </footer>
+            </div>
+          </div>
+          <button
+            className="modal-close is-large"
+            onClick={() => setShowModal(false)}
+          ></button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
